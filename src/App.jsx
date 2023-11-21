4fd0798273fde00f89c6c9af36e58546afe3a255
Elliot Nykvist <elliotnykvist@hotmail.se>
2023-11-21 20:15:50 +0100

    Added a file that creates the header and the page for the cv

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,24 @@ import Settings from './components/Settings';
 import Personal from './components/Personal';
 import School from './components/Personal';
 import Experience from './components/Experience';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <>
       <div className="container">
-        <Settings />
-        <Personal/>
-        <School/>
-        <Experience/>
+        <div className="box-border">
+          <Settings />
+          <Personal/>
+          <School/>
+          <Experience/>
+        </div>
+        <div className="page-div">
+          <div className="page" size="A4">
+            <Header/>
+          </div>
+        </div>
       </div>
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+return <header>
+  <div className="left-header">
+    <img src="sample-avatar.jpg" alt="Avatar" />
+    <h2>Brandon Parker</h2>
+  </div>
+  <div className="right-header">
+    <h3>example@gmail.com</h3>
+    <h3>+44 3560 5566 5566</h3>
+    <h3>London, UK</h3>
+  </div>
+</header>
+}
+
+export default Header;
