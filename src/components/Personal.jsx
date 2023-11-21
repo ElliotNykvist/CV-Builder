6d58ed1618ffa30e2996603754ac031d23b4cd1e
Elliot Nykvist <elliotnykvist@hotmail.se>
2023-11-21 19:49:22 +0100

    Added a components folder and the first input settings html function, also added css

---
+++ src/components/Personal.jsx
@@ -0,0 +1,53 @@
+function Personal() {
+  return <div className="box active">
+  <form className="personal-form">
+    <div className="header1">
+      <h2 className="title">Personal Details</h2>
+      <i className="fa-solid fa-chevron-up down" style={{ color: "white" }} />
+    </div>
+    <div className="input-div1">
+      <label htmlFor="fname">Full name:</label>
+      <input
+        placeholder="Enter Full Name"
+        type="text"
+        id="fname"
+        name="fname"
+        defaultValue="Otto Robinson"
+      />
+    </div>
+    <div className="input-div1">
+      <label htmlFor="email">Email:</label>
+      <input
+        placeholder="Enter Email"
+        type="email"
+        id="email"
+        name="email"
+        defaultValue="example@gmail.com"
+      />
+    </div>
+    <div className="input-div1">
+      <label htmlFor="tel">Phone number:</label>
+      <input
+        placeholder="Enter Your Number"
+        type="tel"
+        id="tel"
+        name="tel"
+        defaultValue="+44 3560 5566 5566"
+      />
+    </div>
+    <div className="input-div1">
+      <label htmlFor="address">Address:</label>
+      <input
+        placeholder="Enter Your Address"
+        type="text"
+        id="address"
+        name="address"
+        defaultValue="London, UK"
+      />
+    </div>
+  </form>
+</div>
+
+}
+
+export default Personal;
---
+++ src/components/Settings.jsx
@@ -0,0 +1,11 @@
+function Settings() {
+  return <div className="settings">
+  <h1>CV Builder</h1>
+  <div>
+    <button id="print">Print</button>
+    <button id="download">Download PDF</button>
+  </div>
+</div>;
+}
+
+export default Settings;
